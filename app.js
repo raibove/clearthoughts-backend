80cfed151b8744dd0cb035e188ffdaadb6c6effd
raibove <shwetakale144@gmail.com>
2022-07-30 12:24:02 +0530

    added get request for all questions

--- app.js
+++ app.js
@@ -31,8 +31,18 @@ app.post('/user', async (req, res) => {
 app.post('/question', async (req,res) => {
   try{
     let data = [req.body.title, req.body.description]
-    let resp = await connection.query('INSERT INTO question (title, description) VALUES (?)', [data])
-    res.send({id: resp[0].insertId})
+    let response = await connection.query('INSERT INTO question (title, description) VALUES (?)', [data])
+    res.send({id: response[0].insertId})
+  }catch(err){
+    res.status(400)
+    res.send(err)
+  }
+})
+
+app.get('/questions', async (req,res) => {
+  try{
+    let response = await connection.query('Select * from question')
+    res.send(response[0])
   }catch(err){
     res.status(400)
     res.send(err)
